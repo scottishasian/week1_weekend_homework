ee54a058d1d72b540bdcad6dcf0031b1f9282c75
Kynan <k.song@blueyonder.co.uk>
2017-09-29 13:32:31 +0100

    Passed test_customer_pet_count.

--- pet_shop.rb
+++ pet_shop.rb
@@ -61,3 +61,7 @@ def add_pet_to_stock(pet_shop, new_pet)
   pet_shop[:pets].push(new_pet)
   return pet_shop[:pets].count()
 end
+
+def customer_pet_count(customers)
+  return customers[:pets].count()
+end
