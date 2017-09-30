d84c8966d526a5168106a48d15d8b5e519365e07
Kynan <k.song@blueyonder.co.uk>
2017-09-30 10:22:13 +0100

    Added notes on what each function does.

--- pet_shop.rb
+++ pet_shop.rb
@@ -4,17 +4,27 @@ def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
 
+# This function works to look within the @pet_shop
+# class/ hash, find the name key pair and return the value.
+
 #Test 2
 
 def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
 end
 
+# Looks to the @pet_shop hash, searches for the :admin hash
+# and then looks for the value of key :total_cash.
+
 #Test 3 + 4
 
 def add_or_remove_cash(pet_shop, amount)
   return pet_shop[:admin][:total_cash] += amount
 end
+
+# Replicates the above function and then will add or
+# subtract an amount based on the argument provided for
+# the amount parameter.
 
 
 #Test 5
@@ -23,17 +33,27 @@ def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
 end
 
+# Within the @pet_shop hash, it finds the :admin hash
+# and returns the value of :pets_sold.
+
 #Test 6
 
 def increase_pets_sold(pet_shop, new_sales)
   return pet_shop[:admin][:pets_sold] += new_sales
 end
 
+# This returns the amount of pets sold, based on
+# the argument provided for the new_sales parameter.
+
 #Test 7
 
 def stock_count(pet_shop)
   return pet_shop[:pets].count()
 end
+
+# This function looks at the @pet_shop hash for the :pets
+# array and uses the .count method to ascertain how many
+# pets/ hashes are within it.
 
 #Test 8 + 9
 
@@ -48,6 +68,15 @@ def pets_by_breed(pet_shop, breed)
   end
   return pets_found
 end
+
+# This function will look for a pet based on the
+# argument provided in the breed parameter.
+# It will run a for loop to check each value within :pets
+# and :breed for a match to the breed argument.
+# if one is found, it is added/pushed to an empty array
+# called pets_found. Finally, pets_found is returned,
+# providing all the information on pets of a specified breed.
+# If the breed is not found the function returns nothing.
 
 #Test 10 + 11
 
@@ -64,6 +93,12 @@ def find_pet_by_name(pet_shop, pet_name)
     # checking the first one.
 end
 
+# This function will iterate through the :pets array
+# to find a match for the name provided for the
+# pet_name parameter. If found, the information on that
+# pet is returned, if not, a nil value is returned.
+
+
 #Test 12
 
 def remove_pet_by_name(pet_shop, pet_name)
@@ -76,12 +111,19 @@ def remove_pet_by_name(pet_shop, pet_name)
   end
 end
 
+# This function will iterate through :pets, checking
+# to find a match for the pet_name parameter.
+# if one is found, its hash is deleted from the :pets
+# array.
+
 #Test 13
 
 def add_pet_to_stock(pet_shop, new_pet)
   pet_shop[:pets].push(new_pet)
   return pet_shop[:pets].count()
 end
+
+
 
 #Test 14
 
